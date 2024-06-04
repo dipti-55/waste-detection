7383ee4c75766a8929e1f32228061ad4de651b9d
Dipti Singh <dipti1010.singh@gmail.com>
2024-06-04 13:46:47 +0530

    project structure and installations

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Dipti Singh',
+    author_email= 'dipti1010.singh@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
